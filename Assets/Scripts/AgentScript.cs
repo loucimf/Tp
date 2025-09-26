cc2c281fc6dac9b43c0c70c280dbf0bcafadfa92
Gexstunf <166758362+Gexstunf@users.noreply.github.com>
2025-09-26 09:51:41 -0300

    push inicial

---
+++ Assets/Scripts/AgentScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentScript : MonoBehaviour
+{
+    NavMeshAgent agent;
+    [SerializeField] Transform targets;
+    [SerializeField] Animator anim;
+    [SerializeField] float velocity;
+
+    
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = targets.position;
+        velocity = agent.velocity.magnitude;
+        anim.SetFloat("Speed",velocity);
+    }
+}
